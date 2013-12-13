bcac6acb9bcdccf97df8def0478032bc7028ed22
Ahmet Bilgili <ahmet.bilgili@epfl.ch>
2013-12-13 14:51:11 +0100

    Complatible with Kitwares FindDevIL

--- Voreen.cmake
+++ Voreen.cmake
@@ -1,7 +1,7 @@
 set(VOREEN_VERSION   4.3 )
 set(VOREEN_REPO_URL  https://github.com/bilgili/Voreen.git )
 set(VOREEN_REPO_TAG bbp )
-set(VOREEN_DEPENDS  Devil )
+set(VOREEN_DEPENDS  DevIL )
 set(VOREEN_NOPACKAGE ON)
 set(VOREEN_NOTEST ON)
 set(VOREEN_OPTIONAL ON)
